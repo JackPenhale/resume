a70e674e7bc57b89d87a13dc337459270fae767f
Your Name <jpenhale@rams.colostate.edu>
2020-08-17 17:49:13 -0600

    Updates

---
+++ precache-manifest.91b84c23db7f2b1bf9ec23120da6dfd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28655c11c2864d961e80650097569aa8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "323a4a7419569e236190",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/portfolio/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "323a4a7419569e236190",
+    "url": "/portfolio/static/js/main.4473221a.chunk.js"
+  },
+  {
+    "revision": "2ec8e9fefc2e47b2dcfc",
+    "url": "/portfolio/static/js/runtime-main.7d360f82.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
