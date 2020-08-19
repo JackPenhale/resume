e31e5f0915dafd8585ac0604056bb4b659febb28
Your Name <jpenhale@rams.colostate.edu>
2020-08-18 20:24:03 -0600

    Updates

---
+++ precache-manifest.d3d67e153f41a93cb5a5e6feb15ce0fb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6f31f1c7bd61452333774681d482a46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4e0b032486a63adf1fb",
+    "url": "/static/css/main.32eb4923.chunk.css"
+  },
+  {
+    "revision": "cc091eadb85d634e634d",
+    "url": "/static/js/2.de7593b5.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.de7593b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4e0b032486a63adf1fb",
+    "url": "/static/js/main.27d9f8cc.chunk.js"
+  },
+  {
+    "revision": "53ff9637c311bd424e1b",
+    "url": "/static/js/runtime-main.5a7750ca.js"
+  },
+  {
+    "revision": "fbe99ddc7f55d3323acc0bcb2ba008c3",
+    "url": "/static/media/hospital.fbe99ddc.png"
+  },
+  {
+    "revision": "3eb4ce0e4889da159031d70b6ba2b218",
+    "url": "/static/media/myface.3eb4ce0e.png"
+  },
+  {
+    "revision": "3f0f0007c44e77179308d5697bd97a33",
+    "url": "/static/media/pi.3f0f0007.png"
+  },
+  {
+    "revision": "8feb0c655f83a6dc040ccb6ea243c41e",
+    "url": "/static/media/tripApp.8feb0c65.png"
+  }
+]);
